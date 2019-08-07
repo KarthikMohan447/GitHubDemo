925d165dff9376ab992b07e7fcadd823c011c0d4
gary.lim <gary.lim@OPT-HO-26S1B92.optimum.net>
2019-08-07 16:49:24 +0800

    Testing withdraw

--- src/Application/PayNowApplication.java
+++ src/Application/PayNowApplication.java
@@ -2,14 +2,21 @@ package Application;
 
 public class PayNowApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 	}
 	
-	void Input() {
+	void Input() 
+	{
 		System.out.println("This is a login method");
 		System.out.println("Version 1");
 		System.out.println("Developer branch");
+	}
+	
+	void withdrawMoney()
+	{
+		System.out.println("testing withdraw");
 	}
 
 }
